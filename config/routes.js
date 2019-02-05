a0dc41b58add6583448d2722a15859a39118b589
hraya <hraya@lssu.edu>
2019-02-04 21:43:02 -0600

    Completed MVP
    completed the login endpoint
    
    the /api/jokes works

--- config/routes.js
+++ config/routes.js
@@ -1,17 +1,15 @@
-const axios = require('axios');
-const db = require('../database/dbConfig.js');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const axios = require("axios");
+const db = require("../database/dbConfig.js");
+const bcrypt = require("bcryptjs");
 
-const { authenticate, generateToken } = require('../auth/authenticate');
+
+const { authenticate, generateToken } = require("../auth/authenticate");
 
 module.exports = server => {
-  server.post('/api/register', register);
-  server.post('/api/login', login);
-  server.get('/api/jokes', authenticate, getJokes);
+  server.post("/api/register", register);
+  server.post("/api/login", login);
+  server.get("/api/jokes", authenticate, getJokes);
 };
-
-
 
 function register(req, res) {
   // implement user registration
@@ -19,39 +17,52 @@ function register(req, res) {
   const hash = bcrypt.hashSync(creds.password, 14);
   creds.password = hash;
   db.insert(creds)
-  .then(ids =>{
-    db.findById(ids[0])
-    .then(user =>{
-      console.log(user)
-      const token = generateToken(user)
-      res.status(201).json({username:user.username, token})
-    }).catch(err =>{
-      console.log('in the second then', err)
-      res.status(500).json({error:"could not make new user"})
+    .then(ids => {
+      db.findById(ids[0])
+        .then(user => {
+          console.log(user);
+          const token = generateToken(user);
+          res.status(201).json({ username: user.username, token });
+        })
+        .catch(err => {
+          console.log("in the second then", err);
+          res.status(500).json({ error: "could not make new user" });
+        });
     })
-  }).catch(err =>{
-    console.log('in the first then', err)
-    res.status(500).json(err)
-  })
-
+    .catch(err => {
+      console.log("in the first then", err);
+      res.status(500).json(err);
+    });
 }
 
 function login(req, res) {
   // implement user login
-
+  const creds = req.body;
+  db.findByUsername(creds.username)
+    .then(user => {
+      if (user && bcrypt.compareSync(creds.password, user.password)) {
+        const token = generateToken(user);
+        res.json({ Welcome: user.username, token });
+      } else {
+        res.status(401).json({ message: "Not Authorized" });
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 }
 
 function getJokes(req, res) {
   const requestOptions = {
-    headers: { accept: 'application/json' },
+    headers: { accept: "application/json" }
   };
 
   axios
-    .get('https://icanhazdadjoke.com/search', requestOptions)
+    .get("https://icanhazdadjoke.com/search", requestOptions)
     .then(response => {
       res.status(200).json(response.data.results);
     })
     .catch(err => {
-      res.status(500).json({ message: 'Error Fetching Jokes', error: err });
+      res.status(500).json({ message: "Error Fetching Jokes", error: err });
     });
 }
--- database/dbConfig.js
+++ database/dbConfig.js
@@ -1,27 +1,23 @@
-const knex = require('knex');
+const knex = require("knex");
 
-const knexConfig = require('../knexfile.js');
+const knexConfig = require("../knexfile.js");
 
 db = knex(knexConfig.development);
 
 module.exports = {
-  
-    insert: (user) =>{
-        return db('users').insert(user);
-    },
-    
-    findByUsername: (username) =>{
-        return db('users').where({username}).first();
-    },
-    
-    findById: (id) =>{
-        return db('users').where({id}).first();
-    },
-    
-    findUsers: ()=>{
-        return db('users').select('id','username')
-    }
-    
-    
-    }
-    
+  insert: user => {
+    return db("users").insert(user);
+  },
+
+  findByUsername: username => {
+    return db("users")
+      .where({ username })
+      .first();
+  },
+
+  findById: id => {
+    return db("users")
+      .where({ id })
+      .first();
+  }
+};
